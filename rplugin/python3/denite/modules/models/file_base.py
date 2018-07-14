59e8ef5f67112dd6af9f0cbb0b3c62dcabb24bd1
Yusuke Mito <y310.1984@gmail.com>
2018-07-14 12:12:34 +0900

    Show file extension

--- rplugin/python3/denite/modules/models/file_base.py
+++ rplugin/python3/denite/modules/models/file_base.py
@@ -7,8 +7,5 @@ class FileBase:
         self.filepath = os.path.normpath(filepath)
 
     def to_filename(self, root_path):
-        filepath = self.remove_base_directory(self.filename_without_extension(), root_path)
+        filepath = self.remove_base_directory(self.filepath, root_path)
         return filepath
-
-    def filename_without_extension(self):
-        return os.path.splitext(self.filepath)[0]
